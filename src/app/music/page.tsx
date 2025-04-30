17e9aef6ce88a259c90f774ffdb281a9ca3e04ae
joaquin <jperaltaperez@uc.cl>
2025-04-30 17:53:38 -0400

    refactor: add nextjs app directory structure

---
+++ src/app/music/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex justify-center items-center  flex-col h-screen bg-amber-700">
+            <p>roahoki está cocinando 🍲...</p>
+        </div>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center  flex-col h-screen font-[family-name:var(--font-geist-sans)] ">
+      <p>roahoki está cocinando 🍲...</p>
+    </div>
+  );
+}
