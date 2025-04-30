25387cc638208b28cf4adc267f8d4551ff8e43e0
joaquin <jperaltaperez@uc.cl>
2025-04-30 18:05:48 -0400

    fix: blogpage now exports a function

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPage() {
+    return (
+        <div className="flex justify-center items-center  flex-col h-screen bg-amber-700">
+            <p>roahoki está cocinando 🍲 en BlogPage...</p>
+        </div>
+    );
+}
--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function MusicPage() {
     return (
         <div className="flex justify-center items-center  flex-col h-screen bg-amber-700">
-            <p>roahoki está cocinando 🍲...</p>
+            <p>roahoki está cocinando 🍲 en MusicPage...</p>
         </div>
     );
 }
