34c790d65eddbc510caee87c087f302a24affaa4
joaquin <jperaltaperez@uc.cl>
2025-05-20 23:48:20 -0400

    add: access to link from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center  flex-col h-screen bg-primary">
+    <div className="flex justify-center items-center flex-col h-screen bg-primary">
       <p>roahoki está cocinando 🍲...</p>
+      <a href="/links">links</a>
     </div>
   );
 }
