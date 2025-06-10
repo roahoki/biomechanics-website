6c0321018b8901a6b75a3cac3029cc1da6cc6850
joaquin <jperaltaperez@uc.cl>
2025-06-10 12:09:25 -0400

    cleaning up unused pages

--- src/app/blog/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function BlogPage() {
-    return (
-        <div className="flex justify-center items-center  flex-col h-screen bg-amber-700">
-            <p>roahoki está cocinando 🍲 en BlogPage...</p>
-        </div>
-    );
-}
--- src/app/music/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function MusicPage() {
-    return (
-        <div className="flex justify-center items-center  flex-col h-screen bg-amber-700">
-            <p>roahoki está cocinando 🍲 en MusicPage...</p>
-        </div>
-    );
-}
